6db92c3162f48782485e854ba1777d0874c21f86
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-10-25 14:09:38 -0400

    removed my object and instead made a function that willis counting all of the words in my html that are selected.

--- javascript/counter.js
+++ javascript/counter.js
@@ -1,4 +1,12 @@
-var wordCounter = {
-  sonnet: document.querySelector('.sonnet').innerText
-};
-console.log(wordCounter);
+// var wordCounter = {
+//   sonnet: document.querySelector('.sonnet').innerText
+//
+// };
+
+
+var sonnet = document.querySelector('.sonnet').innerText
+
+function wordCounter(str) {
+    return str.split(' ').length;
+}
+console.log(wordCounter(sonnet));
